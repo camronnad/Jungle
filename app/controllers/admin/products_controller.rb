0d4736444d5d30e368f87bd4109fc0b39db91f46
Camron Naderi <camronnaderi57@gmail.com>
2023-10-08 12:16:24 -0700

    add username and password authenticattion to Admin::ProductsController

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,4 +38,10 @@ class Admin::ProductsController < ApplicationController
     )
   end
 
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
 end
